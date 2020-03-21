8b58a3c6e8a3efdf3b6390f5f2e48e164f23e6f3
ChangmoSung <changmosung0914@gmail.com>
2020-03-21 12:33:59 -0400

    created contact form

--- script.js
+++ script.js
@@ -55,109 +55,115 @@ portfolio.init = function () {
     // ------------------------------- //
     // ------------project------------ //
     // ------------------------------- //
-    $projects.on('click', 'li', function () {
-        $header.fadeOut();
+    $projects.on('click keypress', 'li', function(e) {
+        if(e.keyCode === 13 || typeof e.keyCode !== 'number') {
 
-        $about.fadeOut();
-        $me.removeClass('imageShown');
-        $description.removeClass('descriptionShown');
-
-        $contact.fadeOut();
-
-
-        // ---------- previously selected project ---------- //
-        if (selectedProject) {
-            previouslySelectedProject = selectedProject;
-
-            selectedProject.children().addClass('notSelected');
-            selectedProject.children().removeClass('selected');
-
-//--------- push it up first and then once it's not shown, push it down--------//
-            setTimeout(function() {
-                previouslySelectedProject.children().removeClass('notSelected');
-            }, 400)
-
-            selectedProject.children().children()[1].children[0].pause();
-            selectedProject.children().children()[1].children[0].currentTime = 0;
-
-            $(list).css('pointer-events', 'auto');
+            $header.fadeOut();
+    
+            $about.fadeOut();
+            $me.removeClass('imageShown');
+            $description.removeClass('descriptionShown');
+    
+            $contact.fadeOut();
+    
+    
+            // ---------- previously selected project ---------- //
+            if (selectedProject) {
+                previouslySelectedProject = selectedProject;
+    
+                selectedProject.children().addClass('notSelected');
+                selectedProject.children().removeClass('selected');
+    
+    //--------- push it up first and then once it's not shown, push it down--------//
+                setTimeout(function() {
+                    previouslySelectedProject.children().removeClass('notSelected');
+                }, 400)
+    
+                selectedProject.children().children()[1].children[0].pause();
+                selectedProject.children().children()[1].children[0].currentTime = 0;
+    
+                $(list).css('pointer-events', 'auto');
+            }
+    
+    
+            // -------- current project selected here ------- //
+            const project = $(this).attr('data-selected');
+            selectedProject = $(project);
+    
+            selectedProject.children().addClass('selected');
+    
+            selectedProject.children().children()[1].children[0].play();
+    
+            // ----------current nav list item selected here----------- //
+            list = $(this);
+            list.css('pointer-events', 'none');
         }
-
-
-        // -------- current project selected here ------- //
-        const project = $(this).attr('data-selected');
-        selectedProject = $(project);
-
-        selectedProject.children().addClass('selected');
-
-        selectedProject.children().children()[1].children[0].play();
-
-        // ----------current nav list item selected here----------- //
-        list = $(this);
-        list.css('pointer-events', 'none');
     })
 
 
     // ---------------------------- //
     // ------------Info------------ //
     // ---------------------------- //
-    $info.on('click', 'li', function () {
-        if (selectedProject) {
-            selectedProject.children().addClass('notSelected');
+    $info.on('click keypress', 'li', function (e) {
+        if(e.keyCode === 13 || typeof e.keyCode !== 'number') {
 
-//--------- push it up first and then once it's not shown, push it down--------//
-            previouslySelectedProject = selectedProject;
-            setTimeout(function() {
-                previouslySelectedProject.children().removeClass('selected');
-                previouslySelectedProject.children().removeClass('notSelected');
-
-// --------to prevent selectedProject from getting class again in the above code-------- //
-                selectedProject = '';
-            }, 400)
-
-            selectedProject.children().children()[1].children[0].pause();
-            selectedProject.children().children()[1].children[0].currentTime = 0;
-
-            $(list).css('pointer-events', 'auto');
-        }
-
-        const selectedList = $(this).attr('data-selected');
-
-        if (selectedList === '.header') {
-            
-            $me.removeClass('imageShown');
-
-            $description.removeClass('descriptionShown');
-            
-            $about.fadeOut(function() {
-                $header.fadeIn('slow');
-            });
-
-            $contact.fadeOut();
-
-        } else if (selectedList === '.about') {
-            $header.fadeOut();
-            
-            $about.css('display', 'flex');
-
-            setTimeout(function() {
-                $me.addClass('imageShown');
-
-                setTimeout(function () {
-                    $description.addClass('descriptionShown');
-                }, 500);
-            },300);
-
-            $contact.fadeOut();
-
-        } else if(selectedList === '.contact'){
-            $header.fadeOut();
-
-            $about.fadeOut();
-            $me.removeClass('imageShown');
-            $description.removeClass('descriptionShown');
-          
-            $contact.fadeIn();
+            if (selectedProject) {
+                selectedProject.children().addClass('notSelected');
+    
+    //--------- push it up first and then once it's not shown, push it down--------//
+                previouslySelectedProject = selectedProject;
+                setTimeout(function() {
+                    previouslySelectedProject.children().removeClass('selected');
+                    previouslySelectedProject.children().removeClass('notSelected');
+    
+    // --------to prevent selectedProject from getting class again in the above code-------- //
+                    selectedProject = '';
+                }, 400)
+    
+                selectedProject.children().children()[1].children[0].pause();
+                selectedProject.children().children()[1].children[0].currentTime = 0;
+    
+                $(list).css('pointer-events', 'auto');
+            }
+    
+            const selectedList = $(this).attr('data-selected');
+    
+            if (selectedList === '.header') {
+                
+                $me.removeClass('imageShown');
+    
+                $description.removeClass('descriptionShown');
+                
+                $about.fadeOut(function() {
+                    $header.fadeIn('slow');
+                });
+    
+                $contact.fadeOut();
+    
+            } else if (selectedList === '.about') {
+                $header.fadeOut();
+                
+                $about.css('display', 'flex');
+    
+                setTimeout(function() {
+                    $me.addClass('imageShown');
+    
+                    setTimeout(function () {
+                        $description.addClass('descriptionShown');
+                    }, 500);
+                },300);
+    
+                $contact.fadeOut();
+    
+            } else if(selectedList === '.contact'){
+                $header.fadeOut();
+    
+                $about.fadeOut();
+                $me.removeClass('imageShown');
+                $description.removeClass('descriptionShown');
+              
+                $contact.fadeIn();
+            }
         }
     })
 }
