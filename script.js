ab4f0ee57e2be7470b98622fc2ac1abee87db765
ChangmoSung <changmosung0914@gmail.com>
2020-03-26 23:49:54 -0400

    landing page construction

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const portfolio = {};
 
 portfolio.init = function () {
     const $welcomeDoor = $('.welcomeDoor');
-    const $navOpener = $('.navOpener');
-    const $nav = $('nav');
-    const $navLists = $('nav li');
-    let navOpened = true;
+    const $mapOpener = $('.mapOpener');
     const $me = $('.me');
     const $aboutContainer = $('.aboutContainer');
     const $contactContainer = $('.contactFlexContainer');
@@ -30,33 +27,11 @@ portfolio.init = function () {
         $(this).css('z-index', '-1');
     })
 
-    $nav.css('top', '0');
-
-    if(navOpened) {
-        $navLists.attr('tabindex', '0');
-    } else {
-        $navLists.attr('tabindex', '-1');
-    }
-
-    $navOpener.on('click', function() {
-        $nav.toggleClass('navOpened');
-        $navOpener.toggleClass('navClosed');
-
-        navOpened = !navOpened;
-        if (navOpened) {
-            $navLists.attr('tabindex', '0');
-            $navOpener.text('hide');
-        } else {
-            $navLists.attr('tabindex', '-1');
-            $navOpener.text('show');
-        }
-    })
-
-    $playButton.on('click', function() {
+    $playButton.on('click', function () {
         const project = $(this).attr('data-project');
         const video = $(`video[data-project=${project}]`);
         const $button = $(this);
-        
+
         if (videoPlaying) {
             video[0].pause();
             $button.html('<img src="assets/play.png" alt=""></img>');
@@ -66,13 +41,11 @@ portfolio.init = function () {
         }
 
         videoPlaying = !videoPlaying;
-    })
+    });
 
-    // ---------------------------- //
-    // ------------Info------------ //
-    // ---------------------------- //
 
-    $cards.on('click keypress', function (e) {
+
+    $mapOpener.on('click', function(e) {
         if (e.keyCode === 13 || typeof e.keyCode !== 'number') {
             if (selectedProject) {
                 // --------- push it up first and then once it's not shown, push it down--------//
@@ -111,8 +84,70 @@ portfolio.init = function () {
                 $contact.fadeOut();
                 $contactContainer.removeClass('contactShown');
 
-
             } else if (selectedList === '.about') {
+                $header.fadeOut();
+
+                $about.css('display', 'flex');
+
+                setTimeout(function () {
+                    $me.addClass('imageShown');
+
+                    $aboutContainer.addClass('aboutContainerShown')
+                }, 50);
+
+                $contact.fadeOut();
+                $contactContainer.removeClass('contactShown');
+
+            } else if (selectedList === '.contact') {
+                $contact.css('display', 'flex').css('z-index', '2');
+
+                setTimeout(function () {
+                    $contactContainer.addClass('contactShown');
+                }, 50)
+
+                $header.fadeOut();
+
+                $about.fadeOut();
+                $me.removeClass('imageShown');
+                $aboutContainer.removeClass('aboutContainerShown');
+            }
+        }
+    })
+
+    // ---------------------------- //
+    // ------------Info------------ //
+    // ---------------------------- //
+
+    $cards.on('click keypress', function (e) {
+        if (e.keyCode === 13 || typeof e.keyCode !== 'number') {
+            if (selectedProject) {
+                // --------- push it up first and then once it's not shown, push it down--------//
+                previouslySelectedProject = selectedProject;
+
+                previouslySelectedProject.children().addClass('notSelected');
+
+                setTimeout(function () {
+                    previouslySelectedProject.children().removeClass('selected');
+                    previouslySelectedProject.children().removeClass('notSelected');
+                    previouslySelectedProject.removeClass('selectedProject');
+
+                    // // --------to prevent selectedProject from getting class again in the above code-------- //
+                    selectedProject = '';
+                }, 250);
+
+                $liveLink.attr('tabindex', '-1');
+                $githubLink.attr('tabindex', '-1');
+                $playButton.attr('tabindex', '-1');
+                $resumeLink.attr('tabindex', '-1');
+                $contactPageLink.attr('tabindex', '-1');
+                $contactSubmitButton.attr('tabindex', '-1');
+
+                $(list).css('pointer-events', 'auto');
+            }
+
+            // ----------- non project list starts here -------------- //
+            const selectedList = $(this).attr('data-selected');
+            if (selectedList === '.about') {
                 $header.fadeOut();
 
                 $about.css('display', 'flex');
@@ -175,7 +210,7 @@ portfolio.init = function () {
             
             const project = $(this).attr('data-selected');
             selectedProject = $(project);
-            
+
             setTimeout(function() {
                 selectedProject.addClass('selectedProject');
                 selectedProject.children().addClass('selected');
